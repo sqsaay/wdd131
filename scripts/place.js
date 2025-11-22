a58f7390d5fcfdb8eff9a9edde46ac9b7f6bcdb4
sqsaay <ayeleth.quevedo@gmail.com>
2025-11-22 15:07:12 -0600

    V02 Country Page - SS

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,22 @@ document.getElementById("currentyear").textContent = currentYear;
 
 // Last update date
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+//Compute Windchill 
+const temperature = 20;
+const wind = 18;
+let result = 0;
+
+
+function calculateWindChill(temp, wind){
+    if (temp <= 10){
+    result = 35.74 + 0.6215 * temp - 35.75 * (wind * 0.16) + 0.4275 * temp * wind * 0.16;
+    return result;
+    }
+    else{
+        result = 'N/A';
+        return result;
+    }
+}
+
+document.getElementById('windChill').textContent = calculateWindChill(temperature, wind);
