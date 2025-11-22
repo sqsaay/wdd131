9b6928c577ad84bdd31c69d029f3291c5fa5304a
sqsaay <ayeleth.quevedo@gmail.com>
2025-11-22 08:41:18 -0600

    V01 - Country Page - SS

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+//Get year
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Last update date
+document.getElementById("lastModified").innerHTML = document.lastModified;
