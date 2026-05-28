477570756beec8f1d1ff8eb0ef106399662b0b1a
sqsaay <ayeleth.quevedo@gmail.com>
2026-05-28 12:50:17 -0600

    DOM events - Update to validate focus on textbox

--- week02/scripts/bom-script.js
+++ week02/scripts/bom-script.js
@@ -1,27 +1,48 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const input = document.querySelector('#favchap');
-  const button = document.querySelector('button');
-  const list = document.querySelector('#list');
+// select elements from the DOM
+const inputElement = document.querySelector("#favchap");
+const buttonElement = document.querySelector("button");
+const listElement = document.querySelector("#list");
 
-  button.addEventListener('click', () => {
-    const chapter = input.value.trim();
+buttonElement.addEventListener("click", () => {
+  if(inputElement.value != ""){
+    const li = document.createElement("li");
+    li.textContent = inputElement.value;
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "✖️";
+    deleteBtn.addEventListener("click", () => {
+      listElement.removeChild(li);
+      inputElement.focus();
+    });
+    li.appendChild(deleteBtn);
+    listElement.appendChild(li);
+    inputElement.value = "";
 
-    if (chapter !== '') {
-      const li = document.createElement('li');
-      const deleteButton = document.createElement('button');
-
-      li.textContent = chapter;
-      deleteButton.textContent = '❌';
-      li.appendChild(deleteButton);
-      list.appendChild(li);
-
-      deleteButton.addEventListener('click', () => {
-        list.removeChild(li);
-      });
-
-      input.value = '';
-    }
-
-    input.focus();
-  });
+  }
+  inputElement.focus();
 });
+
+/* // wait for button clicks
+buttonElement.addEventListener("click", function () {
+	// Check if the user entered something
+	if (inputElement.value != "") {
+		// create list item and give it the value of the input
+		const li = document.createElement("li");
+		li.textContent = inputElement.value;
+		// create a button and add a click event listener
+		const deleteBtn = document.createElement("button");
+		deleteBtn.textContent = "✖️";
+		deleteBtn.addEventListener("click", function () {
+			listElement.removeChild(li);
+			inputElement.focus();
+		});
+		// add the button to the list item
+		li.appendChild(deleteBtn);
+		// OUTPUT: finally display the completed list item to the unordered list
+		listElement.appendChild(li);
+		// clear the user input field
+		inputElement.value = "";
+	}
+	// focus the user back to the input field
+	inputElement.focus();
+});
+ */
